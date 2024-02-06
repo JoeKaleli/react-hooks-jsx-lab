f77c51f8f49ca42f6c170201d14416a1e5c96c53
JoeKaleli <kalelimburu9@gmail.com>
2024-02-06 17:43:59 +0300

    Done With Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { image } from "../data/data";
+import {image} from "../data/data.js"
 
 function About() {
-  return <div>About</div>;
-}
+  return ( <div>
+    <h2>About Me</h2>
+    <p>JoeKaleli</p>
+    <img src={image} alt="I made this"/> </div>
+)}
 
 export default About;
